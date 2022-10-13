7941e7adadbb352bba8e552c15c317321792a8bc
hanifarif81 <hanifarif2815@gmail.com>
2022-10-13 01:33:10 -0400

    Math_problems_Fibonacci_class

--- src/math_problems/Fibonacci.java
+++ src/math_problems/Fibonacci.java
@@ -8,7 +8,25 @@ public class Fibonacci {
      * e.g. - 0,1,1,2,3,5,8,13
      **/
 
-    public static void main(String[] args) {
 
+    static int fibonacci(int num)
+    {
+        if (num <= 1)
+            return num;
+
+        return fibonacci(num -2) + fibonacci(num -3);
+    }
+
+    // Main method
+    public static void main(String args[])
+    {
+        // Given Number N
+        int N = 40;
+
+        // Print the first N numbers
+        for (int i = 0; i < N; i++) {
+            //Print method fibonacci of i and space
+            System.out.print(fibonacci(i) + " ");
+        }
     }
 }
