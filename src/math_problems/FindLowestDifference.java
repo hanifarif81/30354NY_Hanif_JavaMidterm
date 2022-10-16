6ba9d70ead64777aa917b831bc916c184a31d6d0
hanifarif81 <hanifarif2815@gmail.com>
2022-10-16 03:02:16 -0400

    math-problems findMissingNumber

--- src/math_problems/FindLowestDifference.java
+++ src/math_problems/FindLowestDifference.java
@@ -2,7 +2,8 @@ package math_problems;
 
 public class FindLowestDifference {
 
-    /** INSTRUCTIONS
+    /**
+     * INSTRUCTIONS
      * Write a method to return the lowest number, that is not shared between the 2 arrays below
      * HINT: The lowest number that isn't shared between these arrays is 1
      */
@@ -10,11 +11,8 @@ public class FindLowestDifference {
     public static void main(String[] args) {
 
 
-        int[] array1 = {30, 12, 5, 9, 2, 20, 33, 1, -15};
-        int[] array2 = {18, 25, 41, 47, 17, 36, 14, 19, -15};
+        int[] array1 = {30, 12, 5, 9, 2, 20, 33, 1, 15};
+        int[] array2 = {18, 25, 41, 47, 17, 36, 14, 19, 15};
 
     }
-
-
-
 }
--- src/math_problems/FindMissingNumber.java
+++ src/math_problems/FindMissingNumber.java
@@ -8,6 +8,23 @@ public class FindMissingNumber {
 
     public static void main(String[] args) {
         int[] array = new int[] {10, 2, 1, 4, 5, 3, 7, 8, 6};
+        System.out.println("Missing number "+ Search(array,10));
+    }
 
+    public static int Search(int arr[],int n){
+
+        int sumArr = 0;
+        int sumNumber = 0;
+
+        // add all the array elements
+        for(int i=0;i<arr.length;i++){
+            sumArr += arr[i];
+        }
+
+        //add all the numbers from 1-n
+        for(int j=1;j<=n;j++){
+            sumNumber += j;
+        }
+        return  sumNumber -  sumArr;
     }
 }
